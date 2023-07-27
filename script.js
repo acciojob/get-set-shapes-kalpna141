25e8a53b7d8a7c3c39314543384f0b08b705a759
Kalpna <132220523+kalpna141@users.noreply.github.com>
2023-07-27 17:40:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,71 @@
 //complete this code
-class Rectangle {
-	constructor(width, height){
-		this.width=width;
-		this.height=height;
-	}
-	get getwidth(){
-		this.width;
-	}
-	get getheight(){
-		this.height;
-	}
-	getArea()
-	{
-	  return this.width*this.height;	
-	}
-}
-class Square extends Animal {
-	constructor(width, height){
-		super(width, height);
-		this.width=width;
-		this.height=height;
-	}
-	getPerimeter(){
-		 return 2*(this.width+this.height);	
-	}
-}
+// class Rectangle {
+// 	constructor(width, height){
+// 		this.width=width;
+// 		this.height=height;
+// 	}
+// 	get getwidth(){
+// 		this.width;
+// 	}
+// 	get getheight(){
+// 		this.height;
+// 	}
+// 	getArea()
+// 	{
+// 	  return this.width*this.height;	
+// 	}
+// }
+// class Square extends Animal {
+// 	constructor(width, height){
+// 		super(width, height);
+// 		this.width=width;
+// 		this.height=height;
+// 	}
+// 	getPerimeter(){
+// 		 return 2*(this.width+this.height);	
+// 	}
+// }
 // Do not change the code below this line
+class Rectangle {
+  constructor(width, height) {
+    this._width = width;
+    this._height = height;
+  }
+
+  get width() {
+    return this._width;
+  }
+
+  get height() {
+    return this._height;
+  }
+
+  getArea() {
+    return this._width * this._height;
+  }
+}
+
+class Square extends Rectangle {
+  constructor(side) {
+    super(side, side);
+  }
+
+  getPerimeter() {
+    return 4 * this.width;
+  }
+}
+
+// Example usage:
+
+const rectangle = new Rectangle(5, 8);
+console.log('Rectangle Width:', rectangle.width); // Output: 5
+console.log('Rectangle Height:', rectangle.height); // Output: 8
+console.log('Rectangle Area:', rectangle.getArea()); // Output: 40
+
+const square = new Square(6);
+console.log('Square Side:', square.width); // Output: 6
+console.log('Square Area:', square.getArea()); // Output: 36
+console.log('Square Perimeter:', square.getPerimeter()); // Output: 24
+
 window.Rectangle = Rectangle;
 window.Square = Square;
